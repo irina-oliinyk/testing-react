17686d17d4dbcfc3ffb903baa4c0c20d2d23ae63
Iryna Oliinyk <arairishka2@gmail.com>
2024-08-05 22:50:35 +0200

    new bibliotek clsx

---
+++ src/components/Alert/Alert.jsx
@@ -0,0 +1,17 @@
+import './Alert.css'
+import clsx from "clsx";
+
+
+
+export const Alert = ({ variant, outlined, elevated, children }) => {
+  return (
+    <p
+      className={clsx("alert", variant, {
+        "is-outlined": outlined,
+        "is-elevated": elevated,
+      })}
+    >
+      {children}
+    </p>
+  );
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,65 +2,49 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Product from './Product';
-import AppBooks from './books';
-import  Card  from './Card';
-
+// import Product from './Product';
+// import AppBooks from './books';
+// import  Card  from './Card';
+import { Alert } from './Alert/Alert';
 
 
 export default function App() {
   return (
     <div>
-      
-      <Card>
-	     <h1>Card title</h1>
-	     <p>Text between opening and closing tag</p>
-      </Card>
-      <AppBooks />
-      <h1>Products</h1>
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
-      
+      <h1>Card title</h1>
+     <Alert variant="info" outlined>
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error" outlined>
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success" elevated>
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning" outlined elevated>
+        Please update your profile contact information
+      </Alert>
       
     </div>
   );
 }
 
-// function App() {
-//   const [count, setCount] = useState(0)
 
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
 
-// export default App
+//  <Card>
+// 	     <h1>Card title</h1>
+// 	     <p>Text between opening and closing tag</p>
+//       </Card>
+//       <AppBooks />
+//       <h1>Products</h1>
+//       <Product
+//         name="Tacos With Lime"
+//         imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+//         price={10.99}
+//       />
+//       <Product
+//         name="Fries and Burger"
+//         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+//         price={14.29}
+//       />
+      
